c07783ef7b80d7dbad1f60a121a3c96e10a618f3
Mike-Wimmer <53148707+Mike-Wimmer@users.noreply.github.com>
2019-07-23 12:03:21 -0400

    KatController.ino
    Created this file to use the controller to control SmallKat

---
+++ KatController.ino
@@ -0,0 +1,63 @@
+#include <SimplePacketComs.h>
+#include <WiiChuck.h>
+#include <WiFi.h>
+#include <Esp32SimplePacketComs.h>
+#include <EspWii.h>
+#include <ESP32Servo.h>
+#include <Wire.h>
+#include <PacketEvent.h>
+#include <wifi/WifiManager.h>
+#include <server/NameCheckerServer.h>
+
+#define CONTROLLER_ID 2
+#define TEAMNUMBER    22
+String * controllerName = new String("GameController_" + String(TEAMNUMBER));
+
+Accessory classic;
+long timeSincePrint = 0;
+uint8_t valuesLastSent[WII_VALUES_ARRAY_SIZE];
+// SImple packet coms implementation useing WiFi
+static UDPSimplePacket coms;
+// WIfi stack managment state machine
+static WifiManager manager;
+void setup() {
+
+	PacketEventAbstract *ptr = new WiiClassicServerEvent(&classic,
+			CONTROLLER_ID);
+	manager.setup();
+	coms.attach(new NameCheckerServer(controllerName)); // @suppress("Method cannot be resolved")
+	coms.attach(ptr);
+
+}
+
+void loop() {
+	manager.loop();
+	if (manager.getState() == Connected)
+		coms.server();
+	if (millis() - timeSincePrint > 20) {
+		timeSincePrint = millis();
+		classic.readData();
+
+		//classic.printInputs();
+
+		//classic.printInputs();
+		//Serial.print("\r\n took = "+String(millis()-timeSincePrint)+" Values= ");
+		bool print = false;
+		for (int i = 0; i < WII_VALUES_ARRAY_SIZE; i++) {
+			if (valuesLastSent[i]!= classic.values[i]) {
+				print=true;
+			}
+		}
+		if (print) {
+			Serial.print("\r\n Values= ");
+			for (int i = 0; i < 60; i++) {
+				//Serial.print(" , "+String( getControllerStatus()[i]));
+			}
+			for (int i = 0; i < WII_VALUES_ARRAY_SIZE; i++) {
+				Serial.print("\n\tIndex "+String(i)+" = " + String((uint8_t) classic.values[i]));
+				valuesLastSent[i]= classic.values[i];
+			}
+		}
+	}
+
+}
